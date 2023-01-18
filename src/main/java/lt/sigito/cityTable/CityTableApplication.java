fae6f6aef5841c071ceba58dc83f3923d49c2d50
ssiriukaitis <sigitas.siriukaitis@gmail.com>
2023-01-18 13:23:36 +0200

    Initial commit

---
+++ src/main/java/lt/sigito/cityTable/CityTableApplication.java
@@ -0,0 +1,13 @@
+package lt.sigito.cityTable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/sigito/cityTable/CityTableApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.sigito.cityTable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
