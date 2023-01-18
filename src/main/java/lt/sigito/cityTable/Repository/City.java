d00ee61523115e69c26263a5d55e08482a2c70e2
ssiriukaitis <sigitas.siriukaitis@gmail.com>
2023-01-18 14:28:43 +0200

    Automatiniai geteriai ir seteriai

--- src/main/java/lt/sigito/cityTable/Repository/City.java
+++ src/main/java/lt/sigito/cityTable/Repository/City.java
@@ -5,9 +5,18 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 
 @Entity
 @Table(name = "city")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class City {
 
     @Id
@@ -22,56 +31,7 @@ public class City {
     @Column(name = "Population")
     private String population;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCountryCode() {
-        return countryCode;
-    }
-
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
-    }
-
-    public String getDistrict() {
-        return district;
-    }
-
-    public void setDistrict(String district) {
-        this.district = district;
-    }
-
-    public String getPopulation() {
-        return population;
-    }
-
-    public void setPopulation(String population) {
-        this.population = population;
-    }
-
-    @Override
-    public String toString() {
-        return "City{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", countryCode='" + countryCode + '\'' +
-                ", district='" + district + '\'' +
-                ", population='" + population + '\'' +
-                '}';
-    }
 }
 
 
